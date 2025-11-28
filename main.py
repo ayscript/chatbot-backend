47274dbfb887d869a7c4e9eea90cb4bf1dd77970
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-11-28 14:18:23 +0100

    new commit

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def chat(req: ChatRequest):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=True
